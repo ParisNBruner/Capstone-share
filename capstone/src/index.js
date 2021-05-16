8cc82d031e6e55f4dea4ab4bff489b6cb624abd7
ParisNBruner <bittybit99@me.com>
2021-05-16 00:13:37 -0700

    Template added, troubles with page references

--- capstone/src/index.js
+++ capstone/src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Router from "next/router";
 import reportWebVitals from './reportWebVitals';
 import firebase from 'firebase/app'
+import Admin from "./Pages/Admin";
 
 
 // Your web app's Firebase configuration
@@ -19,14 +20,20 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default function Index() {
+    React.useEffect(() => {
+      Router.push("/admin/dashboard");
+        // <Admin/>
+    });
+
+    return <div />;
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
